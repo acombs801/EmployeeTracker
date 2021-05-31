32b384272064e9b3f71abb61a580ea9a344671b5
Adrian <74800016+acombs801@users.noreply.github.com>
2021-05-31 03:24:57 -0700

    finished working server file, removed old server file, fixed sql db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
-USE employee_db;
+USE employeetrackerDB;
 
-INSERT INTO department (name) VALUES ("Human Resources");
-INSERT INTO department (name) VALUES ("Marketing");
-INSERT INTO department (name) VALUES ("Information Technology");
-INSERT INTO department (name) VALUES ("Corporate");
+INSERT INTO department (name) VALUES ("Mail room");
+INSERT INTO department (name) VALUES ("Backend Development");
+INSERT INTO department (name) VALUES ("Testing Offices");
+INSERT INTO department (name) VALUES ("Head Office");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst", 70, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("App Tester", 70, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Postman", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Server Manager", 50, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Ceo", 100, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Ceo", 100, 4);
 
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Larry", "David", 3);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Patrick", "Star", 2);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Mr", "Krabs", 4);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Muhammad", "Wial", 5);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Charles", "Piercer", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Charles", "Mennerick", 1);
--- server.js
+++ server.js
@@ -139,7 +139,6 @@ function addRole() {
               }
           }
           ]) 
-
         .then(function(answer) {
         const department = answer.departmentName;
         connection.query('SELECT * FROM DEPARTMENT', function(err, res) {
@@ -161,4 +160,155 @@ function addRole() {
             })
         })
     })
+}
+
+async function addEmployee() {
+    connection.query('SELECT * FROM role', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([{
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+          }, 
+          {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+          },
+          {
+            name: "roleName",
+            type: "list",
+            message: "What role does the employee have?",
+            choices: function() {
+             rolesArray = [];
+                result.forEach(result => {
+                    rolesArray.push(
+                        result.title
+                    );
+                })
+                return rolesArray;
+              }
+          }
+          ]) 
+        .then(function(answer) {
+        console.log(answer);
+        const role = answer.roleName;
+        connection.query('SELECT * FROM role', function(err, res) {
+            if (err) throw (err);
+            let filteredRole = res.filter(function(res) {
+                return res.title == role;
+            })
+        let roleId = filteredRole[0].id;
+        connection.query("SELECT * FROM employee", function(err, res) {
+                inquirer
+                .prompt ([
+                    {
+                        name: "manager",
+                        type: "list",
+                        message: "Who is your manager?",
+                        choices: function() {
+                            managersArray = []
+                            res.forEach(res => {
+                                managersArray.push(
+                                    res.last_name)
+                                
+                            })
+                            return managersArray;
+                        }
+                    }
+                ]).then(function(managerAnswer) {
+                    const manager = managerAnswer.manager;
+                connection.query('SELECT * FROM employee', function(err, res) {
+                if (err) throw (err);
+                let filteredManager = res.filter(function(res) {
+                return res.last_name == manager;
+            })
+            let managerId = filteredManager[0].id;
+                    console.log(managerAnswer);
+                    let query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+                    let values = [answer.firstName, answer.lastName, roleId, managerId]
+                    console.log(values);
+                     connection.query(query, values,
+                         function(err, res, fields) {
+                         console.log(`You have added this employee: ${(values[0]).toUpperCase()}.`)
+                        })
+                        viewEmployees();
+                        })
+                     })
+                })
+            })
+        })
+})
+}
+
+function updateRole() {
+    connection.query('SELECT * FROM employee', function(err, result) {
+        if (err) throw (err);
+    inquirer
+        .prompt([
+          {
+            name: "employeeName",
+            type: "list",
+            message: "Which employee's role is changing?",
+            choices: function() {
+             employeeArray = [];
+                result.forEach(result => {
+                    employeeArray.push(
+                        result.last_name
+                    );
+                })
+                return employeeArray;
+              }
+          }
+          ]) 
+        .then(function(answer) {
+        console.log(answer);
+        const name = answer.employeeName;
+        /*const role = answer.roleName;
+        connection.query('SELECT * FROM role', function(err, res) {
+            if (err) throw (err);
+            let filteredRole = res.filter(function(res) {
+                return res.title == role;
+            })
+        let roleId = filteredRole[0].id;*/
+        connection.query("SELECT * FROM role", function(err, res) {
+                inquirer
+                .prompt ([
+                    {
+                        name: "role",
+                        type: "list",
+                        message: "What is their new role?",
+                        choices: function() {
+                            rolesArray = [];
+                            res.forEach(res => {
+                                rolesArray.push(
+                                    res.title)
+                                
+                            })
+                            return rolesArray;
+                        }
+                    }
+                ]).then(function(rolesAnswer) {
+                    const role = rolesAnswer.role;
+                    console.log(rolesAnswer.role);
+                connection.query('SELECT * FROM role WHERE title = ?', [role], function(err, res) {
+                if (err) throw (err);
+                    let roleId = res[0].id;
+                    let query = "UPDATE employee SET role_id ? WHERE last_name ?";
+                    let values = [roleId, name]
+                    console.log(values);
+                     connection.query(query, values,
+                         function(err, res, fields) {
+                         console.log(`You have updated ${name}'s role to ${role}.`)
+                        })
+                        viewEmployees();
+                        })
+                     })
+                })
+            
+            //})
+       })
+})
+
 }
